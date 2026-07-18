02dcb7e1468a2b6664b1d2f12b5bf210c97cb0d9
Premanandhan Narayanan <premanandhan@gmail.com>
2026-07-18 10:52:32 +0530

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,10 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     fetch("navigation1.html")
+
         .then(response => response.text())
+
         .then(data => {
-            document.getElementById("navigation-container1").innerHTML = data;
+
+            document.getElementById(
+                "navigation-container1"
+            ).innerHTML = data;
+
+            // Accordion
+
+            const dropdownLinks = document.querySelectorAll(
+                ".dropdown > a"
+            );
+
+            dropdownLinks.forEach(function (link) {
+
+                link.addEventListener("click", function (e) {
+
+                    e.preventDefault();
+
+                    const submenu =
+                        this.nextElementSibling;
+
+                    submenu.classList.toggle("show");
+
+                });
+
+            });
+
         })
+
         .catch(error => {
-            console.error("Error loading navigation:", error);
+
+            console.error(
+                "Error loading navigation:",
+                error
+            );
+
         });
+
 });
